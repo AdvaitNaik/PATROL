e237bbf5a4d25c9191bda55cea54d41e5fe2e40f
AdvaitNaik <advaitnaik99@gmail.com>
2024-04-15 20:59:42 -0700

    deploy

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from src.app import create_patrol_app
 
 app = create_patrol_app()
 
-if __name__ == '__main__':
-    app.run(host="localhost", port=5001)
+# if __name__ == '__main__':
+#     app.run(host="localhost", port=5001)
