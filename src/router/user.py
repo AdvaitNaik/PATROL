3d3af0a838cf14bfc97a7aafa15bbf0380bc0dfc
AdvaitNaik <advaitnaik99@gmail.com>
2024-04-16 22:22:38 -0700

    model

--- src/router/user.py
+++ src/router/user.py
@@ -1,21 +1,78 @@
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response, request, jsonify
 from src.firebase import create_user
+from src.database.model import User
+from src.database.db import db
 
 user_bp = Blueprint('user', __name__)
 
-@user_bp.get('/')
+@user_bp.get('/healthCheck')
 def index():  
     response = Response("User Endpoint")
     response.status_code = 200
     return response
 
-@user_bp.post('/create_user')
-def create_new_user():
+
+@user_bp.get('/<int:user_id>')
+def user(user_id):
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+    
+    user_data = {
+        "username": user.username,
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "phone_number": user.phone_number,
+        "email": user.email,
+        "address": user.address,
+        "city": user.city,
+        "state": user.state,
+        "country": user.country,
+        "zip_code": user.zip_code,
+        "latitude": user.latitude,
+        "longitude": user.longitude,
+        "role_name": user.role.role_name if user.role else None
+    }
+    return jsonify(user_data), 200
+
+
+@user_bp.post('/create')
+def user_create():
     body = request.get_json()
-    message = create_user(body.get("email"), body.get("password"), body.get("fullname"), body.get("claims"))
-    response = Response(message)
-    response.status_code = 200
-    # response.headers.add("Access-Control-Allow-Origin", "*")
-    # response.headers.add('Access-Control-Allow-Headers', "*")
-    # response.headers.add('Access-Control-Allow-Methods', "*")
-    return response
+    try:
+        firebase_user = create_user(body.get("email"), body.get("password"), body.get("username"), body.get("claims"))
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+    
+    new_user = User(
+        username=body.get("username"),
+        first_name=body.get("first_name"),
+        last_name=body.get("last_name"),
+        email=body.get("email"),
+        phone_number=body.get("phone_number", ""),
+        address=body.get("address", ""),
+        city=body.get("city", ""),
+        state=body.get("state", ""),
+        country=body.get("country", ""),
+        zip_code=body.get("zip_code", ""),
+        latitude=body.get("latitude", 0.0),
+        longitude=body.get("longitude", 0.0),
+        role_id=body.get("role_id")
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    
+    return jsonify({"message": "User created successfully", "user_id": new_user.user_id}), 201
+
+
+
+# @user_bp.post('/create_user')
+# def create_new_user():
+#     body = request.get_json()
+#     message = create_user(body.get("email"), body.get("password"), body.get("fullname"), body.get("claims"))
+#     response = Response(message)
+#     response.status_code = 200
+#     response.headers.add("Access-Control-Allow-Origin", "*")
+#     response.headers.add('Access-Control-Allow-Headers', "*")
+#     response.headers.add('Access-Control-Allow-Methods', "*")
+#     return response
