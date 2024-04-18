c74e465ebfa52011efebe9e22291ad96bcccf14f
Roshnee Matlani <roshneematlani21@gmail.com>
2024-04-17 18:26:43 -0700

    added data generator

--- src/database/data.py
+++ src/database/data.py
@@ -3,9 +3,11 @@ from src.database.model import User  # Ensure User class is imported correctly
 from faker import Faker
 import numpy as np
 import random
-
+from src.app import create_patrol_app
 # Initialize Faker
 fake = Faker()
+
+app = create_patrol_app()
 
 # Constants for Los Angeles coordinates
 LA_LAT = 34.0522
@@ -23,12 +25,12 @@ def add_users(num_users):
     for _ in range(num_users):
         lat, lng = generate_nearby_coords(LA_LAT, LA_LNG)
         roles = random.choice([1, 2, 3, 4])
+        phone_number = ''.join(filter(str.isdigit, fake.phone_number()))[:10]  # keep only digits and limit to 10
         user = User(
             username=fake.user_name(),
             first_name=fake.first_name(),
             last_name=fake.last_name(),
-            phone_number=fake.phone_number(),
-            country_code='+1',
+            phone_number=phone_number,  # modified to ensure correct length
             email=fake.email(),
             address=fake.street_address(),
             city='Los Angeles',
@@ -45,6 +47,7 @@ def add_users(num_users):
     return users
 
 if __name__ == '__main__':
+    with app.app_context():
 # Generate data
-    num_users = 1000  # Number of users to generate
-    add_users(num_users)
+        num_users = 100  # Number of users to generate
+        add_users(num_users)
